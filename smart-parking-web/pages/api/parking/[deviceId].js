79aebecefdf03ef7b11aaef0c8f668505dee3bca
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2025-09-20 23:21:22 +0530

    path fix

--- smart-parking-web/pages/api/parking/[deviceId].js
+++ smart-parking-web/pages/api/parking/[deviceId].js
@@ -1,5 +1,5 @@
-import db from '../../../../lib/firebaseAdmin';
-import { validateDeviceId } from '../../../../lib/apiHelpers';
+import db from '../../../lib/firebaseAdmin';
+import { validateDeviceId } from '../../../lib/apiHelpers';
 
 export default async function handler(req, res) {
   const { deviceId } = req.query;
